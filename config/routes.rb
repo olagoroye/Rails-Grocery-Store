d4795738abf96d0929bd68a009ee99a01d0209b5
Olusola Olagoroye <solaolagoroye@gmail.com>
2020-03-16 21:23:54 -0500

    app/assets/stylesheets/items.scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
  
+  resources :list_items
+  resources :lists
+  resources :items
   devise_for :users, :controllers => { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
