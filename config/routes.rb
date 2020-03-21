ce54bb1c06406c39102a5b7936cf3db6fb08426e
Olusola Olagoroye <solaolagoroye@gmail.com>
2020-03-21 15:44:19 -0500

    app/views/layouts/application.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-  resources :list_items do 
+  
     resources :items 
 
-  end
+ resources :user do 
+  resource :list_items
+ end
   
   
   resources :lists 
      
  
 
-  devise_for :users, :controllers => { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' }
-
+  devise_for :users, :controllers => { registrations: 'registrations', omniauth_callbacks: 'users/omniauth_callbacks' } 
+    
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 root to: "home#index"
 end
